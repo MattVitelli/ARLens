d4c67faf9271cc478f44bad5bf11dac38caf212b
Matt Vitelli <matthew.vitelli@gmail.com>
2017-06-30 06:19:11 -0700

    Semi-working version of ARLens - math still has some bugs with rotations + intrinsics

---
+++ Assets/Scripts/Lens.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lens : MonoBehaviour {
+	public Camera camPerson;
+	public Camera camFront;
+	public Transform plane;
+	public Material screenMat;
+	public Matrix4x4 homography;
+
+	Matrix4x4 intrinsicsFront;
+
+
+	Matrix4x4 toNDC(Matrix4x4 src, int width, int height, float znear, float zfar)
+	{
+		Matrix4x4 dst = Matrix4x4.zero;
+		dst.m00 = 2.0f * src.m00 / width;
+		dst.m02 = -(src.m02 / width) + 0.5f;
+		dst.m11 = 2.0f * src.m11 / height;
+		dst.m12 = -(src.m12 / height) + 0.5f;
+		dst.m22 = -(znear + zfar) / (zfar - znear);
+		dst.m23 = -2.0f * znear * zfar / (zfar - znear);
+		dst.m32 = -1.0f;
+		return dst;
+	}
+
+	Matrix4x4 intrinsicsFromFOV(int width, int height, float fovInDegrees)
+	{
+		Matrix4x4 intrinsics = Matrix4x4.identity;
+		intrinsics.m00 = (width * 0.5f) / Mathf.Tan(fovInDegrees * 0.5f * Mathf.Deg2Rad);
+		intrinsics.m02 = width * 0.5f;
+		intrinsics.m11 = (height * 0.5f) / Mathf.Tan(fovInDegrees * 0.5f * Mathf.Deg2Rad);
+		intrinsics.m12 = height * 0.5f;
+		return intrinsics;
+	}
+
+	// Use this for initialization
+	void Start () {
+		int width = 512;
+		int height = 512;
+		camFront.targetTexture = new RenderTexture (width, height, 24, RenderTextureFormat.Default, RenderTextureReadWrite.Default);
+		intrinsicsFront = intrinsicsFromFOV (width, height, camFront.fieldOfView);
+		camFront.projectionMatrix = toNDC (intrinsicsFront, width, height, camFront.nearClipPlane, camFront.farClipPlane);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion rA = camPerson.transform.rotation;
+		Quaternion rB = camFront.transform.rotation;
+		Vector3 tA = camPerson.transform.position;
+		Vector3 tB = camFront.transform.position;
+		Vector3 N = plane.transform.forward;
+		float d = Vector3.Dot (N, plane.transform.position);
+		Matrix4x4 Ra = Matrix4x4.TRS (Vector3.zero, rA, Vector3.one);
+		Matrix4x4 Rb = Matrix4x4.TRS (Vector3.zero, rB, Vector3.one);
+		Vector3 deltaP = (tB - tA) / d;
+		Matrix4x4 inner = Matrix4x4.identity;
+		inner.SetRow (0, new Vector4(1,0,0,0) - (new Vector4 (N.x, N.y, N.z, 0) * deltaP.x));
+		inner.SetRow (1, new Vector4(0,1,0,0) - (new Vector4 (N.x, N.y, N.z, 0) * deltaP.y));
+		inner.SetRow (2, new Vector4(0,0,1,0) - (new Vector4 (N.x, N.y, N.z, 0) * deltaP.z));
+
+		Matrix4x4 intrinsicsPerson = intrinsicsFromFOV (Screen.width, Screen.height, camPerson.fieldOfView);
+		camPerson.projectionMatrix = toNDC (intrinsicsPerson, Screen.width, Screen.height, camPerson.nearClipPlane, camPerson.farClipPlane);
+
+		//homography = intrinsicsFront.inverse * (Ra * (inner * Rb)) * intrinsicsPerson;
+		homography = Ra * (inner * Rb);
+		homography = homography.inverse;
+		screenMat.SetTexture ("_MainTex", camFront.targetTexture);
+		screenMat.SetVector ("_Hba0", homography.GetRow(0));
+		screenMat.SetVector ("_Hba1", homography.GetRow(1));
+		screenMat.SetVector ("_Hba2", homography.GetRow(2));
+		screenMat.SetVector ("_resA", new Vector4(Screen.width, Screen.height,0,0));
+		screenMat.SetVector ("_resB", new Vector4(camFront.targetTexture.width, camFront.targetTexture.height,0,0));
+	}
+}
---
+++ Assets/Shaders/Homography.shader
@@ -0,0 +1,72 @@
+﻿Shader "Unlit/Homography"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_Hba0 ("HBA0", Vector) = (1,0,0,0)
+		_Hba1 ("HBA1", Vector) = (1,0,0,0)
+		_Hba2 ("HBA2", Vector) = (1,0,0,0)
+		_resA ("ResA", Vector) = (1,1,0,0)
+		_resB ("ResB", Vector) = (1,1,0,0)
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float4 uvH : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float3 _Hba0;
+			float3 _Hba1;
+			float3 _Hba2;
+			float2 _resA;
+			float2 _resB;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uvH = o.vertex;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				float3 screenPos = float3(i.uvH.xy, i.uvH.w) / i.uvH.w;
+				screenPos.y *= -1;
+				//screenPos.xy = screenPos.xy*0.5+0.5;
+
+				//screenPos.xy *= _resA;
+				float3 newScreenPos = float3(dot(_Hba0, screenPos), dot(_Hba1, screenPos), dot(_Hba2, screenPos));
+				newScreenPos /= newScreenPos.z;
+				//newScreenPos.xy /= _resB;
+
+				float2 uv = float2(newScreenPos.x, newScreenPos.y) * 0.5 + 0.5;
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, uv);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
